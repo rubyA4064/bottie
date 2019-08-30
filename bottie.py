642122e4dd0ff1e284b0d46d5d1f9f6e2b8af55f
rubyA4064 <54697602+rubyA4064@users.noreply.github.com>
2019-08-30 17:59:18 -0600

    new cipher to break

--- bottie.py
+++ bottie.py
@@ -1,4 +1,3 @@
-import client as client
 import discord
 import os
 TOKEN = os.environ['DISCORD_TOKEN']
@@ -6,89 +5,78 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='$')
 
-async def on_ready():
-    print("OWO TIME")
-    await client.change_presence(game=discord.Game(name=""))
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == "WELOME":
-        await client.send_message(message.channel, "WELOME")
-@bot.command()
-async def testieng(ctx, input: str, seed: int):
-    import random
-    print("Input a string to be encoded:")
-    s = input()
-    print("Input a seed (integer):")
-    seed = int(input())
-    if seed>100:
-        seed=random.randint(0,101)
-    encoded = ""
-    i = 0
-    j = seed
-    for j in range(seed, len(s)):
-        char = chr(int(str(ord(s[j]))[::-1]))
-        encoded = char + encoded
-    for i in range(0, seed):
-        char = chr(int(str(ord(s[i]))[::-1]))
-        encoded = char + encoded
-    print(encoded)
 @bot.command()
 async def rubyscript(ctx, input: str, seed: int):
-    import math
-    print("What would you like to encode?")
+    encode = input.lower()
+    key = input.lower()
 
-    print("What seed(integer) do you want to use?")
-
-
-    firstLength = len(input)
-
-    def reverse_int(n):
-        ans = 0
-        while n > 0:
-            (d, n) = (n % 10, n // 10)
-            ans = 10 * ans + d
-        return (ans)
+    alphabetBase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z']
+    overall = []
 
     def split(word):
         return [char for char in word]
 
-    def swapPos(list, pos1, pos2):
+    def createNum(inputs):
+        inputs1 = []
+        for x in range(0, len(inputs)):
+            inputs1.append(int(ord(inputs[x])) - 97)
+        return inputs1
 
-        list[pos1], list[pos2] = list[pos2], list[pos1]
-        return list
+    def convert(list):
 
-    chars = split(input)
+        # Converting integer list to string list
+        s = [str(i) for i in list]
 
-    for x in range(0, seed):
-        chars.append(chars[0])
-        chars.remove(chars[firstLength])
-    evenOdd = firstLength % 2
-    a = 0
-    b = firstLength - 1
-    chars.reverse()
-    chars1 = []
-    chars2 = []
-    for x in range(0, firstLength):
-        chars1.append(ord(chars[x]))
+        # Join list items using join()
+        res = int("".join(s))
 
-    for x in range(0, firstLength):
-        chars1[x] = reverse_int(chars1[x])
-    for x in range(0, firstLength):
-        chars1[x] = chr(chars1[x])
+        return (res)
 
-    output11="".join(chars1)
+    for x in range(0, 25):
+        overall.append(alphabetBase)
 
-    output = output11
-    await ctx.send('`' + output + '`')
+        alphabetBase = alphabetBase[1:] + alphabetBase[:1]
+
+    encodeL = split(encode)
+    keyL0 = split(key)
+
+    for x in range(0, len(encode)):
+        if ' ' in encodeL:
+            encodeL.remove(' ')
+    for x in range(0, len(key)):
+        if ' ' in keyL0:
+            keyL0.remove(' ')
+
+    keyL = []
+    eLen = len(encode)
+    kLen = len(key)
+
+    index = 0
+    # copies key to be same length as plaintext
+    for x in range(0, kLen):
+        keyL.append(keyL0[x])
+
+    while kLen < eLen:
+
+        keyL.append(keyL0[index])
+        kLen += 1
+        index += 1
+        if index >= len(key):
+            index = 0
+
+    keyL = createNum(keyL)
+    encode = createNum(encode)
+
+    output = []
+    for x in range(0, eLen):
+        output.append(overall[keyL[x]][encode[x]])
+
+    print(''.join(output))
 
 client = discord.Client()
 
 @bot.event
-async def testing(ctx, input: str, seed: int):
-
-    output="boooooop"
-    await ctx.send('`' + output + '`')
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
